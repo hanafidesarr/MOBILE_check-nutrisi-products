66190f2d9d7dadf3c8c8b897e702ee30aa246ac9
hanafi desar <hanafidesar@hanafis-MacBook-Pro.local>
2023-12-08 08:39:04 +0700

    change name app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'cek nilai gizi',
+  appName: 'NutriCheck',
   webDir: 'www',
   server: {
     androidScheme: 'https'
